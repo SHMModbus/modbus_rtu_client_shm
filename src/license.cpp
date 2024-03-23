de8ab6610535eb8aaa56b0886fe3472339a82497
Nikolas Koesling <nikolas@koesling.info>
2024-03-23 23:45:27 +0100

    add semaphore library

--- src/license.cpp
+++ src/license.cpp
@@ -1272,5 +1272,35 @@ void print_licenses(std::ostream &o, bool full) {
     }
     o << "\n";
     o << "\n";
+    // #################################################################################################################
+    // #################################################################################################################
+    // cxxsemaphore
+    // #################################################################################################################
+    // #################################################################################################################
+    o << "cxxsemaphore Library (https://github.com/NikolasK-source/cxxsemaphore)\n";
+    o << "\n";
+    o << "    MIT License\n";
+    if (full) {
+        o << "\n";
+        o << "    Copyright (c) 2023 Nikolas Koesling\n";
+        o << "\n";
+        o << "    Permission is hereby granted, free of charge, to any person obtaining a copy\n";
+        o << "    of this software and associated documentation files (the \"Software\"), to deal\n";
+        o << "    in the Software without restriction, including without limitation the rights\n";
+        o << "    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n";
+        o << "    copies of the Software, and to permit persons to whom the Software is\n";
+        o << "    furnished to do so, subject to the following conditions:\n";
+        o << "\n";
+        o << "    The above copyright notice and this permission notice shall be included in all\n";
+        o << "    copies or substantial portions of the Software.\n";
+        o << "\n";
+        o << "    THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n";
+        o << "    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n";
+        o << "    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n";
+        o << "    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n";
+        o << "    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n";
+        o << "    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n";
+        o << "    SOFTWARE.\n";
+    }
     o.flush();
 }
